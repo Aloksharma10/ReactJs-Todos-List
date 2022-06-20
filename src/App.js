a83095c801d79545703524f36796061e585e901f
Aloksharma10 <92917515+Aloksharma10@users.noreply.github.com>
2022-06-20 17:32:07 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -22,20 +22,13 @@ function App() {
 
 
   const onDelete = (todo) => {
-    console.log("I am ondelete of todo", todo);
-    // Deleting this way in react does not work
-    // let index = todos.indexOf(todo);
-    // todos.splice(index, 1);
-
-    setTodos(todos.filter((e) => {
+     setTodos(todos.filter((e) => {
       return e !== todo;
     }));
-    console.log("deleted", todos)
     localStorage.setItem("todos", JSON.stringify(todos));
   }
 
   const addTodo = (title, desc) => {
-    console.log("I am adding this todo", title, desc)
     let sno;
     if (todos.length === 0) {
       sno = 0;
@@ -49,7 +42,6 @@ function App() {
       desc: desc,
     }
     setTodos([...todos, myTodo]);
-    console.log(myTodo);
   }
 
   const [todos, setTodos] = useState(initTodo);
@@ -60,7 +52,7 @@ function App() {
   return ( 
     <> 
     <Router>
-      <Header title="My Todos List" searchBar={false} /> 
+      <Header title="Todos List" searchBar={false} /> 
       <Switch>
           <Route exact path="/" render={()=>{
             return(
